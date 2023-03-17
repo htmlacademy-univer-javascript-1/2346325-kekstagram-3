1495530255e0c76aefb7f90e53cbea755088c7a2
semen <semehvor@gmail.com>
2023-03-17 15:41:44 +0300

    fix task 4

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
 function generateRandomNumber(min, max) {
-    if (min > max) {
-        throw new Error('Min value cant be more max value');
-    }
-    return Math.round(Math.random() * (max - min) + min);
+  if (min > max) {
+      throw new Error('Min value cant be more max value');
+  }
+  return Math.round(Math.random() * (max - min) + min);
 }
 
 function isLengthCorrect(str, maxLength) {
-    return str.length <= maxLength;
+  return str.length <= maxLength;
 }
 
 function generateData() {
-    const photos = new Array();
+  const photos = new Array();
     for (let i = 1; i <= 25; i++) {
-        const photo = {
-            id: i,
-            url: `photos/${i}.jpg`,
-            description: `Photo number ${i}`,
-            likes: generateRandomNumber(15, 200),
-            comments: generateRandomNumber(0, 200),
+      const photo = {
+        id: i,
+        url: `photos/${i}.jpg`,
+        description: `Photo number ${i}`,
+        likes: generateRandomNumber(15, 200),
+        comments: generateRandomNumber(0, 200),
         }
-
-        photos.push(photo);
-    }
-    return photos;
+      photos.push(photo);
+  }
+  return photos;
 }
 
 generateData();
