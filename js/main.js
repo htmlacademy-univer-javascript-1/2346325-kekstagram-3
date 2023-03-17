6cd5a6e15408a7948c65488a19db2144fcd2af7f
semen <semehvor@gmail.com>
2023-03-17 15:43:33 +0300

    fix identation again

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function generateRandomNumber(min, max) {
   if (min > max) {
-      throw new Error('Min value cant be more max value');
+    throw new Error('Min value cant be more max value');
   }
   return Math.round(Math.random() * (max - min) + min);
 }
