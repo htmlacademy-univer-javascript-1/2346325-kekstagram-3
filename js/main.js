6bac340fa3085f8c4f5c6ddca0a037cca8bca74a
semen <semehvor@gmail.com>
2023-03-17 15:26:11 +0300

    task 4

--- js/main.js
+++ js/main.js
@@ -8,3 +8,22 @@ function generateRandomNumber(min, max) {
 function isLengthCorrect(str, maxLength) {
     return str.length <= maxLength;
 }
+
+function generateData() {
+    const photos = new Array();
+
+    for (let i = 1; i <= 25; i++) {
+
+        const photo = {
+            id: i,
+            url: `photos/${i}.jpg`,
+            description: `Photo number ${i}`,
+            likes: generateRandomNumber(15, 200),
+            comments: generateRandomNumber(0, 200),
+        }
+
+        photos.push(photo);
+    }
+
+    return photos;
+}
