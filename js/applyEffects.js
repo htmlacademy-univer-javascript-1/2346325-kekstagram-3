9be9be2884a28b845e4af2784f8f731ae2fc450a
semen <semehvor@gmail.com>
2023-04-13 14:48:39 +0300

    fix effects applying

--- js/applyEffects.js
+++ js/applyEffects.js
@@ -1,9 +1,52 @@
 const imgPreview = document.querySelector('.img-upload__preview');
-let effect = 'none';
+let currentEffect = 'none';
 const radios = document.querySelectorAll('.effects__radio');
 const sliderElement = document.querySelector('.effect-level__slider');
 const valueElement = document.querySelector('.effect-level__value');
 const form = document.getElementById('upload-select-image');
+
+const effects  = [
+  {
+    name: 'chrome',
+    cssEffect: 'grayscale',
+    rangeMin: 0,
+    rangeMax: 1,
+    step: 0.1,
+    postfix: ''
+  },
+  {
+    name: 'sepia',
+    cssEffect: 'sepia',
+    rangeMin: 0,
+    rangeMax: 1,
+    step: 0.1,
+    postfix: ''
+  },
+  {
+    name: 'marvin',
+    cssEffect: 'invert',
+    rangeMin: 0,
+    rangeMax: 100,
+    step: 1,
+    postfix: '%'
+  },
+  {
+    name: 'phobos',
+    cssEffect: 'blur',
+    rangeMin: 0,
+    rangeMax: 3,
+    step: 0.1,
+    postfix: 'px'
+  },
+  {
+    name: 'heat',
+    cssEffect: 'brightness',
+    rangeMin: 1,
+    rangeMax: 3,
+    step: 0.1,
+    postfix: ''
+  },
+];
 
 radios.forEach((radio) => {
   radio.addEventListener('input', () => {
@@ -13,55 +56,30 @@ radios.forEach((radio) => {
 
 function setSlider() {
   // if there is no effect hide slider and delete styles
-  if (effect === 'none') {
+  if (currentEffect === 'none') {
     sliderElement.classList.add('hidden');
     imgPreview.style.removeProperty('filter');
   } else {
     sliderElement.classList.remove('hidden');
-  }
 
-  // set slider options depending on  effect
-  if (effect === 'marvin') {
-    sliderElement.noUiSlider.updateOptions({
-      range: {
-        min: 0,
-        max: 100
-      },
-      start: 100,
-      step: 1
-    });
-  } else if (effect === 'phobos') {
-    sliderElement.noUiSlider.updateOptions({
-      range: {
-        min: 0,
-        max: 3
-      },
-      start: 3,
-      step: 0.1
-    });
-  } else if (effect === 'heat') {
-    sliderElement.noUiSlider.updateOptions({
-      range: {
-        min: 1,
-        max: 3
-      },
-      start: 3,
-      step: 0.1
-    });
-  } else {
-    sliderElement.noUiSlider.updateOptions({
-      range: {
-        min: 0,
-        max: 1
-      },
-      start: 1,
-      step: 0.1
+    // set slider options depending on  effect
+    effects.map((effect) => {
+      if (effect.name === currentEffect) {
+        sliderElement.noUiSlider.updateOptions({
+          range: {
+            min: effect.rangeMin,
+            max: effect.rangeMax,
+          },
+          start: effect.rangeMax,
+          step: effect.step,
+        });
+      }
     });
   }
 }
 
 function updateEffect(radio) {
-  effect = radio.value;
+  currentEffect = radio.value;
   setSlider();
 }
 
@@ -78,23 +96,18 @@ noUiSlider.create(sliderElement, {
 sliderElement.noUiSlider.on('update', () => {
   valueElement.value = sliderElement.noUiSlider.get();
   const value = valueElement.value;
-  if (effect === 'chrome') {
-    imgPreview.style.filter = `grayscale(${value})`;
-  } else if (effect === 'sepia') {
-    imgPreview.style.filter = `sepia(${value})`;
-  } else if (effect === 'marvin') {
-    imgPreview.style.filter = `invert(${value}%)`;
-  } else if (effect === 'phobos') {
-    imgPreview.style.filter = `blur(${value}px)`;
-  } else if (effect === 'heat') {
-    imgPreview.style.filter = `brightness(${value})`;
-  }
+
+  effects.map((effect) => {
+    if (currentEffect === effect.name) {
+      imgPreview.style.filter = `${effect.cssEffect}(${value}${effect.postfix})`;
+    }
+  });
 });
 
 function resetImage() {
   sliderElement.classList.add('hidden');
   imgPreview.removeAttribute('style');
-  effect = 'none';
+  currentEffect = 'none';
 }
 
 form.addEventListener('reset', resetImage);
