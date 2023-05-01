688b5b6c71bc90b3131d1cbfdb93d4ffd782165c
semen <semehvor@gmail.com>
2023-05-01 12:03:36 +0300

    work with server

--- js/drawPhotos.js
+++ js/drawPhotos.js
@@ -1,18 +1,20 @@
-import { generateData } from './data.js';
-
-const photos = generateData();
+import { getData } from './network.js';
+import { createErrorMessage } from './uploadForm.js';
 
 const imageTemplate = document.querySelector('#picture');
 const picturesList = document.querySelector('.pictures');
 
-const partPicturesList = document.createDocumentFragment();
+function drawPhotos(photos) {
+  const partPicturesList = document.createDocumentFragment();
+  photos.forEach(({url, likes, comments}) => {
+    const photo = imageTemplate.content.cloneNode(true);
+    photo.querySelector('.picture__img').src = url;
+    photo.querySelector('.picture__comments').textContent = comments;
+    photo.querySelector('.picture__likes').textContent = likes;
+    partPicturesList.appendChild(photo);
+  });
 
-photos.forEach(({url, likes, comments}) => {
-  const photo = imageTemplate.content.cloneNode(true);
-  photo.querySelector('.picture__img').src = url;
-  photo.querySelector('.picture__comments').textContent = comments;
-  photo.querySelector('.picture__likes').textContent = likes;
-  partPicturesList.appendChild(photo);
-});
+  picturesList.append(partPicturesList);
+}
 
-picturesList.append(partPicturesList);
+getData(drawPhotos);
